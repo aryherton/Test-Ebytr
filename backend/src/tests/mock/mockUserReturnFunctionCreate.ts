7e383b9454bc9a044e38a5807a234f9b810ed857
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-06-25 20:03:29 -0300

    feat: created file "mockUserReturnFuntionCreate.ts"

---
+++ backend/src/tests/mock/mockUserReturnFunctionCreate.ts
@@ -0,0 +1,15 @@
+export default {
+    _id: {
+      $oid: "62b7028859d9b4b2bb1197e0"
+    },
+    name: "Ary",
+    email: "test@gmail.com",
+    password: "123456",
+    arrMessage: [
+      {
+        id: 1,
+        description: "Mensagem test",
+        status: "pendente"
+      }
+    ]
+  }
