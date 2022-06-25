118b6b08008a0288692cfd697f43fdf53844917a
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-06-25 20:00:37 -0300

    feat: created file "messageRes.ts" "./backend/src/enum/MessageRes.ts"

---
+++ backend/src/enum/messageRes.ts
@@ -0,0 +1,11 @@
+enum Messages {
+  INCORRECT = 'Incorrect email or password',
+  INVALID = 'All fields must be filled',
+  UPDATE_SUCCESS = 'Match updated successfully',
+  SUCCESS_RECORD = 'User registered successfully',
+  LOGIN_SUCCESS = 'Login realized success',
+  ERROR_SERVER = 'Sorry error found',
+  BODY_IS_REQUIRED = 'Body is required in the request'
+}
+
+export default Messages;
