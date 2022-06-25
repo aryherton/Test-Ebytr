07d295ed346e38ce20907540d3cc3a0d791dd56b
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-06-25 10:07:28 -0300

    fix: change database name in "URL" file "./backend/src/database/model/Connections.ts

--- backend/src/database/model/Connections.ts
+++ backend/src/database/model/Connections.ts
@@ -2,4 +2,4 @@ import { connect } from 'mongoose';
 
 import options from '../config';
 
-export default (url: string = 'mongodb://localhost:27017/my-mongo') => connect(url, options);
+export default (url: string = 'mongodb://localhost:27017/Ebytr') => connect(url, options);
