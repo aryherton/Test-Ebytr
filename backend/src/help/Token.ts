94009fd7b9e0c1c5e6b7dfb9219461edf4800542
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-06-26 13:44:58 -0300

    feat: created file "Token.ts" in folder "./backend/src/help"

---
+++ backend/src/help/Token.ts
@@ -0,0 +1,33 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default class Token {
+  private _email: string;
+  private _password: string;
+  private _secretKey: string = process.env.SECRET_KEY || 'ebytrKey';
+
+  constructor(email: string, password: string) {
+    this._email = email;
+    this._password = password;
+  }
+
+  createdToken = () => {
+    try {
+      return jwt
+        .sign(
+          {
+            email: this._email,
+            password: this._password,
+          },
+          this._secretKey,
+          { expiresIn: '31d' },
+        );
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        console.log('Class Token :', error.message);
+      }
+    }
+  };
+}
