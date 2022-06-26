1aa48d53cf65881efff532e10f570d42e001a9a8
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-06-26 20:35:21 -0300

    fix: renamed "userServ" in file "./backend/src/controller/LoginCtrll.ts"

--- backend/src/controller/LoginCtrll.ts
+++ backend/src/controller/LoginCtrll.ts
@@ -5,17 +5,17 @@ import StatusHttp from '../enum/statusHttp';
 import MessagesRes from '../enum/messageRes';
 
 export default class LoginCtrll {
-  constructor(private userSerc = new UserService()) {}
+  constructor(private userServ = new UserService()) {}
 
   createLogin = async (req: Request, res: Response): Promise<Response> => {
     try {
       const { name, email, password } = req.body;
-      const checkEmail = await this.userSerc.getUserByEmail(email);
+      const checkEmail = await this.userServ.getUserByEmail(email);
 
       if (checkEmail.length) {
         return res.status(StatusHttp.UNAUTHORIZED).json({ message: MessagesRes.ALREADY_REGISTERED });
       }
-      const token = await this.userSerc.insertUser({ name, email, password });
+      const token = await this.userServ.insertUser({ name, email, password });
 
       return res.status(StatusHttp.CREATED).json({ message: MessagesRes.LOGIN_SUCCESS, token });
     } catch(e) {
@@ -27,13 +27,13 @@ export default class LoginCtrll {
  checkLogin =  async (req: Request, res: Response): Promise<Response> => {
     try {
         const { email, password } = req.body;
-        const token = await this.userSerc.validUser(email, password);
+        const token = await this.userServ.validUser(email, password);
 
         if (token) {
           return res.status(StatusHttp.OK).json({ message: MessagesRes.LOGIN_SUCCESS, token });
         }
 
-      return res.status(StatusHttp.NOT_FOUND).json({ message: MessagesRes.BODY_IS_REQUIRED });
+      return res.status(StatusHttp.UNAUTHORIZED).json({ message: MessagesRes.INCORRECT });
     } catch(e) {
       console.log(e);
       return res.status(StatusHttp.SERVER_ERROR).json({ message: MessagesRes.ERROR_SERVER});
