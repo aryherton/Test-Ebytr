2f5498e0ab034ea701b4da73f6d7a692c889aa39
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-06-26 21:28:48 -0300

    feat: cheking token validation

--- backend/src/controller/MessageCtrll.ts
+++ backend/src/controller/MessageCtrll.ts
@@ -3,6 +3,7 @@ import { Request, Response } from 'express';
 import StatusHttp from '../enum/statusHttp';
 import Messages from '../enum/messageRes';
 import IUser from '../database/interface/IUser';
+import Token from '../help/Token';
 import MessageServ from '../services/Message';
 
 export default class MessageCtrll {
@@ -10,11 +11,21 @@ export default class MessageCtrll {
 
   async setMessageCtrll(req: Request, res: Response) {
     try {
-        const { email } = req.body;
-        const messageServ = new MessageServ();
-        await messageServ.setMessage(email, req.body);
+        const { authorization } = req.headers;
+        
+        if (authorization) {
+          const token = new Token();
+          token.validToken(authorization);
+
+          const { email } = req.body;
+          const messageServ = new MessageServ();
+
+          await messageServ.setMessage(email, req.body);
 
         return res.status(StatusHttp.OK).json({ message: Messages.UPDATE_SUCCESS });
+        }
+
+        return res.status(StatusHttp.NO_CONTENT).json({ message: Messages.NOT_TOKEN });
     } catch(e) {
         console.log(e);
         return res.status(StatusHttp.SERVER_ERROR).json({ message: Messages.ERROR_SERVER });
