4202be9432c81b6250acdeb9930070f31741447f
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-06-27 16:31:21 -0300

    feat: import "JwtPayload" in file "./backend/src/controller/MessageCtrll.ts"

--- backend/src/controller/MessageCtrll.ts
+++ backend/src/controller/MessageCtrll.ts
@@ -1,10 +1,13 @@
 import { Request, Response } from 'express';
 
+import BuildArrMessagens from '../services/BuildArrMessages';
 import StatusHttp from '../enum/statusHttp';
 import Messages from '../enum/messageRes';
 import IUser from '../database/interface/IUser';
+import IMessage from '../database/interface/IMessage';
 import Token from '../help/Token';
 import MessageServ from '../services/Message';
+import { JwtPayload } from 'jsonwebtoken';
 
 export default class MessageCtrll {
   // constructor(private messageServ = new MessageServ()) {}
@@ -12,15 +15,17 @@ export default class MessageCtrll {
   async setMessageCtrll(req: Request, res: Response) {
     try {
         const { authorization } = req.headers;
+        const message: IMessage = req.body.message;
         
         if (authorization) {
           const token = new Token();
-          token.validToken(authorization);
-
-          const { email } = req.body;
+          const user = token.validToken(authorization) as JwtPayload;
           const messageServ = new MessageServ();
+          const buildArrMess = new BuildArrMessagens();
+          const arrMessage: IMessage[] = await buildArrMess
+            .insertMessageArr(user.email, message) as IMessage[];
 
-          await messageServ.setMessage(email, req.body);
+          await messageServ.setMessage(user.email, arrMessage);
 
         return res.status(StatusHttp.OK).json({ message: Messages.UPDATE_SUCCESS });
         }
